1e62dec6ffe6b0030679c0d12d160231e8fa0caa
pshreya747 <pshreya0711@gmail.com>
2024-12-29 20:50:57 +0530

    schemas added

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,10 @@
+import {z} from 'zod';
+
+
+export const messageSchema=z.object({
+    content: z
+    .string()
+    .min(10, {message: 'Content must be atleast of 10 characters'})
+    .max(300, {message: 'Content should not be more than 300 characters'})
+   
+})
