501b8d0c6f0508c57215a82f6131d7c8875faab5
pshreya747 <pshreya0711@gmail.com>
2025-01-07 18:34:02 +0530

    Fixed bugs in navbar and message cards .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AuthProvider from "@/context/AuthProvider";
+import "./globals.css";
+import AuthProvider from "../context/AuthProvider";
 import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "True Feedback",
+  title: "Mystry Messages",
   description: "Real feedback from real people.",
 };
 
